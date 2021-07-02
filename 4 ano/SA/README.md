e08ae8fc8e6b4281aa070c0598243e95df9d5043
Rafael Lourenço <36737793+rafael4512@users.noreply.github.com>
2021-07-02 15:12:55 +0100

    Delete 4 ano/SA directory

--- 4 ano/SA/README.md
+++
@@ -1,9 +0,0 @@
-# SA_TP
-
-## Sistemas Autónomos ##
-- Trabalhos práticos:
-- [x] Sistemas Autónomos
-- [x] Sensorização Ambiente
-	- Raspberry Pi a recolher dados + Drive para manter os dados
-	- Google Data Studio para representar os dados
-	- Modelo preditivo, e comparar com os dados reais previstos
--- 4 ano/SA/RoboCode/meleerumble.bat
+++
@@ -1,9 +0,0 @@
-@REM
-@REM Copyright (c) 2001-2020 Mathew A. Nelson and Robocode contributors
-@REM All rights reserved. This program and the accompanying materials
-@REM are made available under the terms of the Eclipse Public License v1.0
-@REM which accompanies this distribution, and is available at
-@REM https://robocode.sourceforge.io/license/epl-v10.html
-@REM
-
-java -Xmx1024M -cp libs/robocode.jar;libs/roborumble.jar;libs/bcel-6.2.jar;libs/codesize-1.2.jar -XX:+IgnoreUnrecognizedVMOptions "--add-opens=java.base/sun.net.www.protocol.jar=ALL-UNNAMED" "--add-opens=java.base/java.lang.reflect=ALL-UNNAMED" "--add-opens=java.desktop/sun.awt=ALL-UNNAMED" roborumble.RoboRumbleAtHome ./roborumble/meleerumble.txt
